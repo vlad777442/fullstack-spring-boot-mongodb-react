04ccffd0295c4317a15beeaccddbf9603a630a32
vlad777442 <60163112+vlad777442@users.noreply.github.com>
2023-06-12 11:46:59 +0300

    first commit

---
+++ src/main/java/com/moviemanager/movies/MovieController.java
@@ -0,0 +1,27 @@
+package com.moviemanager.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+@CrossOrigin(origins = "*") // to work with react on local server
+public class MovieController {
+    @Autowired
+    private MovieService movieService;
+    @GetMapping
+    public ResponseEntity<List<Movie>> allMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.getAllMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId) {
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), HttpStatus.OK);
+    }
+}
---
+++ src/test/java/com/moviemanager/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviemanager.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
